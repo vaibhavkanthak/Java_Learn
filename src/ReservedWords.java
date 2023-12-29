08881edc6af55552f23ad0454309c0cbe8cbd94b
vaibhavkanthak <vaibhavkanthak@gmail.com>
2023-12-29 12:54:27 +0530

    more

---
+++ src/ReservedWords.java
@@ -0,0 +1,46 @@
+public class ReservedWords {
+
+        public static void main(String[] args) {
+
+            int x = 10;
+            System.out.println(x);
+            System.out.println("Hello World");
+
+            /*
+             * Reserved words/Keywords are predefined and have special meaning in java
+             * language
+             * These words cannot be used as identifiers(such as class names, menthod names,
+             * and variable names)
+             * As they are reserved for specific purposes in java programming language.
+             * There are total 53 reserved words in Java
+             * Out of which there are 50 keywords and 3 reserved literals.
+             * When reserved word is associated with any functionality then it is called as
+             * Keyword in Java. And when reserved word is used to represent value then it is
+             * called as Reserved Literals(true, false, and null).
+             * There are 2 unused keywords in Java (const and goto)
+             * There are 48 used keywords in Java.
+             * keywords for data types-> byte, short, int, long, float, double, boolean,
+             * char. (8)
+             * keywords for flow control-> if, else, switch, case, default, do, while, for,
+             * break, continue, return.(11)
+             * keywords for modifiers-> public, private, protected, static, final
+             * abstract, synchronized, native, strictfp, transient, volatile (11)
+             * Keywords for Exception Handling-> try, catch, finally, throw, throws, assert.
+             * (6)
+             * Keywords for Class-> class, interface, extends, implements, package, import.
+             * (6).
+             * Keywords for Object-> new, instanceof, super, this. (4)
+             * Keywords for return type-> void. (1)
+             * true and false are values for boolean data types.
+             * And null is value for Object reference.
+             * The remaining used keyword is 'enum' which is used to define a group of named
+             * constants.
+             *
+             * Conclusions->All reserved words in java contains only lower case alphabets.
+             * There is no delete keyword in Java because destruction of useless objects is
+             * the responsibility of garbage collector.
+             *
+             */
+        }
+
+}
